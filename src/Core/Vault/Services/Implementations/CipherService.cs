98d651d92ec43a060ebc3d75e893e38cf20f8c6b
T.M <tony.magi@bravurasecurity.com>
2023-05-25 14:42:52 -0400

    BSAF-63 correct upload limit to 1.8 for sends as well as ciphers

--- src/Core/Vault/Services/Implementations/CipherService.cs
+++ src/Core/Vault/Services/Implementations/CipherService.cs
@@ -938,7 +938,7 @@ namespace Bit.Core.Vault.Services;
 
             if (storageBytesRemaining < requestLength)
             {
-                throw new BadRequestException("Not enough storage available.");
+                throw new BadRequestException("Not enough storage available for attachment.");
             }
         }
 
@@ -1017,7 +1017,7 @@ namespace Bit.Core.Vault.Services;
             var storageAdjustment = attachments?.Sum(a => a.Value.Size) ?? 0;
             if (org.StorageBytesRemaining() < storageAdjustment)
             {
-                throw new BadRequestException("Not enough storage available for this tean.");
+                throw new BadRequestException("Not enough storage available for this team.");
             }
 
             ValidateCipherLastKnownRevisionDateAsync(cipher, lastKnownRevisionDate);
