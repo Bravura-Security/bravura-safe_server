1772db53b6bebef9dbc189a59fbf158b931bfe8f
Richard Lee <richard.lee@bravurasecurity.com>
2024-01-05 17:39:45 -0500

    BSAF-141
    Fix login

--- src/Core/Entities/User.cs
+++ src/Core/Entities/User.cs
@@ -231,7 +231,7 @@ public class User : ITableObject<Guid>, ISubscriber, IStorable, IStorableSubscri
 
     public User Decrypt(byte[] cryptKey, byte[] authKey)
     {
-        if (encrypted == false)
+        if (encrypted == false || encrypted is null)
             return this;
         if (!string.IsNullOrWhiteSpace(MasterPasswordHint)) MasterPasswordHint = AESHMACEncryption.SimpleDecrypt(MasterPasswordHint, cryptKey, authKey);
         if (!string.IsNullOrWhiteSpace(TwoFactorRecoveryCode)) TwoFactorRecoveryCode = AESHMACEncryption.SimpleDecrypt(TwoFactorRecoveryCode, cryptKey, authKey);
