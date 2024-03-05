e1b955f3a3f6029d0f6a5c7ee8e9d57111fd694a
T.M <tony.magi@bravurasecurity.com>
2024-03-05 12:20:47 -0500

    BSAF-204 update version numbers

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using Microsoft.Extensions.Localization;
 
 [assembly: ResourceLocation("Resources")]
 [assembly: RootNamespace("Bit.Core")]
-[assembly: AssemblyDescription("2023-13.1")]
+[assembly: AssemblyDescription("2024-1.0")]
