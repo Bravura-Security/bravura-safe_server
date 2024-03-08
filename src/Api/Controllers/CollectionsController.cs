00a495fa52036527895fe7af0118910999ec5dbe
Richard Lee <richard.lee@bravurasecurity.com>
2024-03-08 15:19:46 -0500

    BSAF-141 Fixed version and right length of password hints

--- src/Api/Controllers/CollectionsController.cs
+++ src/Api/Controllers/CollectionsController.cs
@@ -123,6 +123,13 @@ public class CollectionsController : Controller
             return await GetManyWithDetails_vNext(orgId);
         }
 
+        var currentContextOrganization = _currentContext.GetOrganization(orgId);
+        if (currentContextOrganization.Type == OrganizationUserType.User) //user level == User type
+        {}
+
+        else if (currentContextOrganization.Type == OrganizationUserType.Custom) //user type === custom
+        {}
+        else
         // Old pre-flexible collections logic follows
         if (!await ViewAtLeastOneCollectionAsync(orgId) && !await _currentContext.ManageUsers(orgId) && !await _currentContext.ManageGroups(orgId))
         {
