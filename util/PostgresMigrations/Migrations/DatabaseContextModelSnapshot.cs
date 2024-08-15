a2f6a9905f28b840e3568df757b0171598d992be
Gissela Campos <gissela.camposvillegas@bravurasecurity.com>
2024-08-15 14:45:19 -0400

    BSAF-263 Fixes to support Postgres.

--- util/PostgresMigrations/Migrations/DatabaseContextModelSnapshot.cs
+++ util/PostgresMigrations/Migrations/DatabaseContextModelSnapshot.cs
@@ -1703,7 +1703,7 @@ namespace Bit.PostgresMigrations.Migrations
                     b.ToTable("ProjectSecret");
                 });
 
-            modelBuilder.Entity("AmazonSNSDevice", b =>
+            modelBuilder.Entity("Bit.Infrastructure.EntityFramework.Models.AmazonSNSDevice", b =>
                 {
                     b.Property<long?>("Id")
                         .HasColumnType("bigint");
@@ -1726,7 +1726,7 @@ namespace Bit.PostgresMigrations.Migrations
                     b.ToTable("AmazonSNSDevice");
                 });
 
-            modelBuilder.Entity("HubConnection", b =>
+            modelBuilder.Entity("Bit.Infrastructure.EntityFramework.Models.HubConnection", b =>
                 {
                     b.Property<long?>("Id")
                         .HasColumnType("bigint");
